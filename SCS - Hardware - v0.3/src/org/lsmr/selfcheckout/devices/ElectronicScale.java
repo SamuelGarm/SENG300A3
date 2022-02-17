287f697fb41415baeaa302f52d922e8320975caa
samuel Osweiler <32417612+SamuelGarm@users.noreply.github.com>
2022-02-16 23:02:53 -0700

    Added 100% coverage tests

--- SCS - Hardware - v0.3/src/org/lsmr/selfcheckout/devices/ElectronicScale.java
+++ SCS - Hardware - v0.3/src/org/lsmr/selfcheckout/devices/ElectronicScale.java
@@ -92,10 +92,11 @@ public class ElectronicScale extends AbstractDevice<ElectronicScaleObserver> {
 	 * 
 	 * @param item
 	 *            The item to add.
+	 * @throws Exception 
 	 * @throws SimulationException
 	 *             If the same item is added more than once.
 	 */
-	public void add(Item item) {
+	public void add(Item item) throws Exception {
 		if(phase == Phase.ERROR)
 			throw new SimulationException(new IllegalStateException(
 				"This method may not be used when the device is in an erroneous operation phase."));
@@ -130,6 +131,7 @@ public class ElectronicScale extends AbstractDevice<ElectronicScaleObserver> {
 		if(phase == Phase.ERROR)
 			throw new SimulationException(new IllegalStateException(
 				"This method may not be used when the device is in an erroneous operation phase."));
+
 		if(phase == Phase.CONFIGURATION)
 			throw new SimulationException(
 				new IllegalStateException("This method may not be called during the configuration phase."));
